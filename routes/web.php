d289d942a9b683224c96001bcab899e738555701
3B032123 <3b032123@gm.student.ncut.edu.tw>
2023-12-06 22:23:32 +0800

    七個路由分別改用七個Route::敘述一一寫出

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 //Route::resource('Products', ProductController::class);
-Route::resource('Products', ProductController::class)->only([
-    'index', 'show', 'store', 'update', 'destroy'
-]);
+//Route::resource('Products', ProductController::class)->only([
+//    'index', 'show', 'store', 'update', 'destroy'
+//]);
+Route::get('products', [ProductController::class,'index'])->name("products.index");
+Route::get('products/{product}', [ProductController::class,'show'])->name("products.show");
+Route::get('products/create', [ProductController::class,'create'])->name("products.create");
+Route::post('products', [ProductController::class,'store'])->name("products.store");
+Route::get('products/{product}/edit', [ProductController::class,'edit'])->name("products.edit");
+Route::patch('products/{product}', [ProductController::class,'update'])->name("products.update");
+Route::delete('products/{product}', [ProductController::class,'destroy'])->name("products.destroy");
 //每一個路由名稱所用的URL、HTTP方法(或稱HTTP動詞)、與所串接的控制器&方法
 //products.index    URL:products                HTTP方法:GET|HEAD 控制器&方法:ProductController@index
 //products.show     URL:products/{product}      HTTP方法:GET|HEAD 控制器&方法:ProductController@show
