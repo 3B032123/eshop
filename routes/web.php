040748509c33446518fcadf5201e96516db3aa8d
3B032123 <3b032123@gm.student.ncut.edu.tw>
2023-12-06 22:11:16 +0800

    七個Products路由應該有的作用

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('Products', ProductController::class);
-//  每一個路由名稱所用的URL、HTTP方法(或稱HTTP動詞)、與所串接的控制器&方法
+//每一個路由名稱所用的URL、HTTP方法(或稱HTTP動詞)、與所串接的控制器&方法
 //products.index    URL:products                HTTP方法:GET|HEAD 控制器&方法:ProductController@index
 //products.show     URL:products/{product}      HTTP方法:GET|HEAD 控制器&方法:ProductController@show
 //products.create   URL:products/create         HTTP方法:GET|HEAD 控制器&方法:ProductController@create
@@ -33,3 +33,11 @@ Route::resource('Products', ProductController::class);
 //  PUT|PATCH       Products/{Product} ...................... Products.update › ProductController@update
 //  DELETE          Products/{Product} .................... Products.destroy › ProductController@destroy
 //  GET|HEAD        Products/{Product}/edit ..................... Products.edit › ProductController@edit
+//七個Products路由應該有的作用
+//products.index    作用:顯示所有有的產品資料
+//products.show     作用:顯示指定的產品資料
+//products.create   作用:顯示新增產品資料的頁面
+//products.store    作用:products.create的產品資料新增到資料庫
+//products.edit     作用:編輯指定的產品資料
+//products.update   作用:更新指定的產品資料
+//products.destroy  作用:刪除指定的產品資料
